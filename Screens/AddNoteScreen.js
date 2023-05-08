df8cb166b49b61e5db9207bf0c3c651cd6bb76d6
Ayush757 <ayushhh.a757@gmail.com>
2023-05-08 13:49:40 +0530

    Firebase Login Authentication added

--- Screens/AddNoteScreen.js
+++ Screens/AddNoteScreen.js
@@ -1,8 +1,49 @@
 import * as React from "react";
-import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider,View } from "native-base";
-import { TextInput } from "react-native";
-
+import {  Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider,View,Select, Box, CheckIcon} from "native-base";
+import { Alert, TextInput } from "react-native";
+import { getFirestore } from 'firebase/firestore';
+import { doc, setDoc ,collection} from "firebase/firestore"; 
+import {app} from "../Firebase/Firebase.config"
+import {auth} from "../Firebase/Firebase.config"
 const AddNoteScreen = () => {
+  const [priority, setPriority] = React.useState("")
+  const [color, setColor] = React.useState("")
+  const [title,setTitle]=React.useState("")
+  const[description,setDescription]=React.useState("")
+  const handleSaveNote = async () => {
+    if(priority==0){
+      setColor("#91F48F")
+    }
+    else if(priority==1){
+      setColor("#FFF599")
+    }
+    else if(priority==2){
+      setColor("#FF9E9E")
+    }
+    try {
+      const db = getFirestore(app);
+      const currentUser = auth.currentUser;
+      if (!currentUser) {
+        throw new Error("User not found");
+      }
+      const userId = currentUser.uid;
+      const notesRef = collection(db, "users", userId, "notes");
+      const newNote = {
+        title: title,
+        description: description,
+        priority: priority,
+        color:color
+      };
+      const docRef = doc(notesRef);
+      await setDoc(docRef, newNote);
+      Alert.alert("Note added successfully!");
+      setTitle("");
+      setDescription("");
+      setPriority("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <Center w="100%">
       <Box safeArea p="2" py="8" w="90%" maxW="290">
         <Heading size="lg" fontWeight="600" color="coolGray.800" _dark={{
@@ -18,12 +59,22 @@ const AddNoteScreen = () => {
 
         <VStack space={5} mt="5">
          <View>
-            <TextInput placeholder="Title" multiline={true} style={{fontSize:30}} maxLength={20}></TextInput>
+            <TextInput placeholder="Title" multiline={true} style={{fontSize:30}} maxLength={20} onChangeText={(text)=>setTitle(text)} value={title}></TextInput>
          </View>
          <View>
-            <TextInput placeholder="Description" multiline={true} style={{fontSize:20}} maxLength={200}></TextInput>
+            <TextInput placeholder="Description" multiline={true} style={{fontSize:20}} maxLength={200}onChangeText={(text)=>setDescription(text)} value={description}></TextInput>
          </View>
-          <Button mt="2" colorScheme="indigo">
+         <Box maxW="300">
+        <Select selectedValue={priority} minWidth="200" accessibilityLabel="Choose Priority" placeholder="Choose Priority" _selectedItem={{
+        bg: "teal.600",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} onValueChange={itemValue => setPriority(itemValue)}>
+          <Select.Item label="High" value="2" />
+          <Select.Item label="Medium" value="1" />
+          <Select.Item label="Low" value="0" />
+        </Select>
+      </Box>
+          <Button onPress={handleSaveNote} mt="2" colorScheme="indigo">
         Save
           </Button>
           
--- Screens/NotesScreen.js
+++ Screens/NotesScreen.js
@@ -1,22 +1,49 @@
-import React from "react"
+import React, { useState,useEffect } from "react"
 import { SafeAreaView, StatusBar, TouchableOpacity } from "react-native"
 import { View,Text } from "react-native"
 import { AntDesign } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 import NoteItem from "../Components/NoteItem"
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useNavigation } from "@react-navigation/native";
+import { getFirestore } from 'firebase/firestore';
+import { doc, setDoc ,getDocs,collection} from "firebase/firestore"; 
+import {app} from "../Firebase/Firebase.config"
+import {auth} from "../Firebase/Firebase.config"
 
 const NotesScreen=()=>{
     const navigation=useNavigation()
     const Drawer = createDrawerNavigator();
+    const [notes, setNotes] = useState([]);
+    
+  
+    useEffect(() => {
+        const fetchNotes = async () => {
+            const db = getFirestore(app);
+            const currentUser = auth.currentUser;
+          if (!currentUser) return;
+    
+          const userId = currentUser.uid;
+          const notesRef = collection(db, 'users', userId, 'notes');
+          const notesSnapshot = await getDocs(notesRef);
+          const notesData = notesSnapshot.docs.map(doc => doc.data());
+          const sortedNotesData = notesData.sort((a, b) => b.priority - a.priority);
+            setNotes(sortedNotesData);
+          
+        };
+    
+        fetchNotes();
+      }, []);
+
+      console.log(notes)
+    
     return(
 
     <SafeAreaView style={{alignItems:"center",height:"100%"}}>
         <StatusBar/>
         <View style={{flexDirection:"row",alignItems:"center",marginBottom:20}}>
         <TouchableOpacity onPress={() => navigation.openDrawer()} style={{backgroundColor:"black",height:40,width:40,alignItems:"center",justifyContent:"center",borderRadius:10,marginRight:"5%"}}>
-            <Feather name="menu" size={24} color="white" />
+        <Entypo name="menu" size={24} color="white" />
             </TouchableOpacity>
             <Text style={{color:"black",fontSize:40,marginRight:"40%"}}>Notes</Text>
             <TouchableOpacity onPress={()=>{navigation.navigate("SearchScreen")}} style={{backgroundColor:"black",height:40,width:40,alignItems:"center",justifyContent:"center",borderRadius:10}}>
